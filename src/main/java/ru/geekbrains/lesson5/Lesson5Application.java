17987783db40b530bd5f7ac57ff59d2b46012476
iddqd <simondo@inbox.ru>
2021-06-17 21:40:58 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/lesson5/Lesson5Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.lesson5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson5Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/lesson5/Lesson5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.lesson5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
